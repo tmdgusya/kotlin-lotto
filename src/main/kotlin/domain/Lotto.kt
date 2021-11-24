f97d56602f3ebcb485f9c2f457c883b0d983cfff
roach <tmdgusya@naver.com>
2021-11-24 21:17:18 +0900

    로또 기본금액을 상수로 추출

--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -2,7 +2,9 @@ package domain
 
 import generator.RandomLottoNumberGenerator
 
+const val DEFAULT_LOTTO_FEE = 5000
+
 class Lotto(private val randomLottoNumberGenerator: RandomLottoNumberGenerator) {
     val numbers: IntArray = randomLottoNumberGenerator.generateNumbers()
-    val fee: Int = 5000
+    val fee: Int = DEFAULT_LOTTO_FEE
 }
